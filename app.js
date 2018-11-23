92b9fdab11aac651214244fe1c35dab13380da82
do7be <bomsap700@gmail.com>
2018-11-23 16:17:44 +0900

    webpack dev server on express

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const route = require('./routes')
+
+const app = express()
+app.set('view engine', 'pug')
+
+app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+if (process.env.NODE_ENV === 'development') {
+  const webpack = require('webpack')
+  const webpackConfig = require('./webpack.config')
+  const compiler = webpack({ mode: 'development', ...webpackConfig })
+
+  app.use(
+    require('webpack-dev-middleware')(compiler, {
+      noInfo: true,
+      publicPath: webpackConfig.output.publicPath
+    })
+  )
+  app.use(require('webpack-hot-middleware')(compiler))
+}
+
+app.use('/', route)
+
+// eslint-disable-next-line no-console
+app.listen(3000, () => console.log('App listening on port 3000!'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,21 @@
+const webpack = require('webpack')
 const path = require('path')
 
 module.exports = {
-  entry: './app/src/index.jsx',
+  devtool: 'eval-source-map',
+  entry: {
+    index: [
+      'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
+      './app/src/index.jsx'
+    ]
+  },
   output: {
     path: path.resolve(__dirname, 'app', 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].bundle.js'
   },
   devServer: {
-    contentBase: path.resolve(__dirname, 'app', 'dist'),
-    host: '0.0.0.0',
-    port: '8080',
-    open: true,
-    hot: true
+    contentBase: path.resolve(path.join(__dirname, 'public')),
+    port: 3000
   },
   module: {
     rules: [
@@ -29,5 +33,5 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx']
   },
-  plugins: []
+  plugins: [new webpack.HotModuleReplacementPlugin()]
 }
